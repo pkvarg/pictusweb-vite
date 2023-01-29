c6f4419d9bcbbbbe4e798991800af865282e5e42
Peter Varga <pkvarg@yahoo.se>
2023-01-29 19:58:21 +0100

    services ?

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -6,9 +6,9 @@ const Services = () => {
         Weby, ktoré Vám prinesú úspech
       </h1>
       <div className='border border-white border-3 rounded-[15px] mt-[120px] max-w-[75%] mx-auto'>
-        <div className='flex lg:flex-row flex-col items-center '>
+        <div className='flex lg:flex-row flex-col items-center py-16'>
           <div className='w-[50%] ml-[10%]'>
-            <img className='' src='/services.png' alt='services' />
+            <img className='w-[60%]' src='/design.png' alt='services' />
           </div>
 
           <div className='w-[50%] mr-[5%] flex flex-col'>
@@ -18,8 +18,8 @@ const Services = () => {
             </div>
             <div>
               <p className='text-[2rem]'>
-                Vytvoríme Vám jedinečné logo, použiteľné na webstránke, v
-                podpise emailu či vizitke.
+                Logo použiteľné na webstránke, v podpise emailu či vizitke.
+                Jedinečné. Vaše.
               </p>
               {/* <p className='text-[2rem]'>
                
@@ -31,8 +31,7 @@ const Services = () => {
             </div>
             <div>
               <p className='text-[2rem]'>
-                Chcete prezentovať svoje služby, hobby alebo robiť vážny biznis?
-                Na štýle záleží.
+                Prezentujete služby, hobby alebo vážny biznis? Na štýle záleží.
               </p>
               {/* <p className='text-[2rem]'>
                 
@@ -41,23 +40,40 @@ const Services = () => {
           </div>
         </div>
 
-        {/* <div className=''>
-          <img className='w-[5%]' src='/service-check.png' alt='service' />
-          <h3 className='text-[30px] mt-[18px]'>Non-stop prevádzka webu</h3>
+        {/* bottom */}
+        <div className='flex lg:flex-row flex-col items-center py-16'>
+          <div className='w-[50%] ml-[5%] flex flex-col'>
+            <div className='flex flex-row items-center gap-3'>
+              <img className='w-[5%]' src='/service-check.png' alt='service' />
+              <h3 className='text-[3rem] mt-[18px]'>Non-stop prevádzka webu</h3>
+            </div>
+            <div>
+              <p className='text-[2rem]'>
+                Rýchly a spoľahlivý web fungujúci na PC, mobile aj tablete.
+                Non-stop.
+              </p>
+              {/* <p className='text-[2rem]'>
+               
+              </p> */}
+            </div>
+            <div className='flex flex-row items-center gap-3'>
+              <img className='w-[5%]' src='/service-check.png' alt='service' />
+              <h3 className='text-[3rem] mt-[18px]'>Keď nie ste na príjme</h3>
+            </div>
+            <div>
+              <p className='text-[2rem]'>
+                Formulár Vám zabezbečí, že správy od zákazníkov prídu vždy do
+                Vašej e-pošty.
+              </p>
+              {/* <p className='text-[2rem]'>
+                
+              </p> */}
+            </div>
+          </div>
+          <div className='w-[50%] ml-[12.5%]'>
+            <img className='w-[75%]' src='/server.png' alt='server' />
+          </div>
         </div>
-        <div>
-          <p className='text-[20px]'>Garantujeme rýchly a spoľahlivý web</p>
-          <p className='text-[20px]'>fungujúci na PC, mobile aj tablete.</p>
-        </div>
-        <div className=''>
-          <img className='w-[5%]' src='/service-check.png' alt='service' />
-          <h3 className='text-[30px] mt-[18px]'>Keď nie ste na príjme</h3>
-        </div>
-        <div>
-          <p className='text-[20px]'>Samozrejmosťou je formulár, ktorý Vám</p>
-          <p className='text-[20px]'>zabezbečí, že si správy od zákazníkov</p>
-          <p>môžete nájsť priamo vo Vašej e-pošte.</p>
-        </div> */}
       </div>
     </div>
   )
