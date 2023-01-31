6addd3f1d8b8050cab77012b9b03b8b984a4b6d2
Peter Varga <pkvarg@yahoo.se>
2023-01-31 13:19:36 +0100

    footer mob ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,33 +5,46 @@ import { footerVariants } from '../motion'
 
 const Footer = () => (
   <>
-    <div>
+    <div className='mx-3'>
       <motion.footer
         variants={footerVariants}
         initial='hidden'
         whileInView='show'
-
-        // className={`${styles.xPaddings} py-8 relative`}
       >
-        <div className='footer-gradient' />
-        <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-          <div className='flex items-center justify-between flex-wrap gap-5'></div>
+        {/* <div className='footer-gradient' /> */}
+        <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8 `}>
+          <div className='mb-[10px] h-[2px] bg-white opacity-10' />
 
           <div className='flex flex-col'>
-            <div className='mb-[50px] h-[2px] opacity-10' />
+            <div className='flex lg:flex-row flex-col items-center justify-between flex-wrap gap-4'>
+              <div className='flex lg:flex-row flex-col gap-2 justify-center items-center'>
+                <h4 className='font-extrabold text-[24px] flex-nowrap text-white'>
+                  Copyright &copy; {Date().substring(11, 15)}
+                </h4>
+                <h4 className='font-extrabold text-[24px] text-white'>
+                  Pictusweb s.r.o.
+                </h4>
+              </div>
 
-            <div className='flex items-center justify-between flex-wrap gap-4'>
-              <h4 className='font-extrabold text-[24px] text-white'>
-                METAVERUS
-              </h4>
-              <p className='font-normal text-[14px] text-white opacity-50'>
-                Copyright © 2021 - 2022 Metaversus. All rights reserved.
+              <a className='font-extrabold text-[24px] text-white' href='/gdpr'>
+                GDPR
+              </a>
+              <a
+                className='font-extrabold text-[24px] text-white'
+                href='/trade-rules'
+              >
+                Obchodné podmienky
+              </a>
+              <p className='font-normal text-[24px] text-white opacity-50'>
+                WhatsApp: +421 904 798 505
+                <br />
+                <a href='mailto:info@pictusweb.sk'>Email: info@pictusweb.sk</a>
               </p>
             </div>
           </div>
         </div>
       </motion.footer>
-      <div className='bg:hero-gradient h-20'></div>
+      <div className='bg:hero-gradient h-10'></div>
     </div>
   </>
 )
