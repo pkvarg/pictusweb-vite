4f41c81d6ced5e5099145e2de485ae3c2e7a89dc
peter varga <pkvarg@yahoo.se>
2023-03-18 12:54:13 +0100

    get Bots from server

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,16 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/NavbarProjects'
-import { useStateContext } from '../context/StateContext'
+import axios from 'axios'
 
 const Counter = () => {
-  const { botsCount } = useStateContext()
+  const [count, setCount] = useState()
+
+  const getBots = async () => {
+    const { data } = await axios.post(`http://localhost:1000/api/bots/increase`)
+    console.log(data)
+    setCount(data)
+  }
+  getBots()
 
   return (
     <>
       <div className='second-gradient text-white h-[100vh]'>
         <Navbar />
         <div className='p-8' id='counter'>
-          Bots unsuccessful:
+          Bots unsuccessful: {count}
         </div>
       </div>
     </>
